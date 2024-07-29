b0ead94e9f76183bed05a0835086ed82afa9076b
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-07-29 09:36:49 +0000

    corregido

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
     // Retornamos el valor en dólares
     return valueInDollar;
 };
 const fromDollarToYen = function(valueInDollar) {
     // Convertimos el valor a dólares
-    let valueInYen = valueInDollar * 156.5;
+    let valueInYen = valueInDollar * (oneEuroIs.JPY / oneEuroIs.USD);
     // Retornamos el valor en dólares
     return valueInYen;
 };
 const fromYenToPound = function(valueInYenes) {
     // Convertimos el valor a dólares
-    let valueInPound = valueInYenes * 0.87;
+    let valueInPound = valueInYenes * (oneEuroIs.GBP / oneEuroIs.JPY);
     // Retornamos el valor en dólares
     return valueInPound;
 };
