837877568cac7c48b041c3d3ec53a635494cd97c
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-08-05 09:11:06 +0000

    corregido4.0

--- test.js
+++ test.js
@@ -1,54 +1,19 @@
- // Importar la función sum del archivo app.js
-const { sum, fromDollarToYen } = require('./app.js');
-
-
-test('adds 14 + 9 to equal 23', () => {
-
-    let total = sum(14, 9);
-
-  
-    expect(total).toBe(23);
-}); 
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require("./app.js");
 
 test("One euro should be 1.07 dollars", function() {
-   
-    const { fromEuroToDollar } = require('./app.js');
-
-   
     const dollars = fromEuroToDollar(1);
-
-   
     const expected = 1 * 1.07;
-
-    
-    expect(fromEuroToDollar(dollars)).toBe(expected); 
+    expect(dollars).toBe(expected);
 });
 
-test("One dollar should be 156.5 yenes", function() {
-    
-    const { fromDollarToYen } = require('./app.js');
-
-    
-    const yenes = fromDollarToYen(1);
-
-   
-    const expected = (1/1.07) * 156.5;
-
-    
-    expect(fromDollarToYen(yenes)).toBe(expected); 
+test("1 dollar should be approximately 146.4486 yen", function() {
+    const yen = fromDollarToYen(1);
+    const expected = (1 / 1.07) * 156.5;
+    expect(yen).toBeCloseTo(expected);
 });
 
-
-test("One yen should be 0.87 pounds", function() {
- 
-    const { fromYenToPound } = require('./app.js');
-
-  
+test("1 yen should be approximately 0.00556 pounds", function() {
     const pounds = fromYenToPound(1);
-
-    
     const expected = (1 / 156.5) * 0.87;
-
-    
-    expect(fromYenToPound(pounds)).toBe(expected); 
+    expect(pounds).toBeCloseTo(expected);
 });
